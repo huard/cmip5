c8af0142d3edee4d94338015ace4acc49c91868b
David Huard <david.huard@gmail.com>
2012-09-19 09:11:20 -0400

    Climatological means with inline concatenation.'

--- cmip5/cmds.py
+++ cmip5/cmds.py
@@ -13,22 +13,24 @@ responsible to launch those on their system.
 
 """
 
-# TODO: Use CDO rather than NCO to concatenate records (one dependency less).
-# TODO: Convert to nc4 and compress while concatenating. 
-
 import esg
 import os, warnings, glob
 from general import NestedDict
 
-def concatenate(path):
+def cluster_time_slices(path):
     """Identify all files within the directory `path` that belong to the
     same simulation and that could be concatenated along their record 
-    dimension, ie time, concatenate them and remove the original files.
+    dimension (time). 
     
     Parameters
     ----------
     path : str
       Path to a directory containing netCDF datasets.
+
+    Returns
+    -------
+    A NestedDict holding a sequence of files belonging to the same simulation 
+    and variable. 
     """
     F = NestedDict()
     files = glob.glob(os.path.join(path, '*.nc'))
@@ -48,7 +50,19 @@ def concatenate(path):
             F[variable][MIPtable][model][experiment][ensemble] = []
     
         F[variable][MIPtable][model][experiment][ensemble].append(f)
-        
+
+    return F
+
+
+
+def concatenate(path):
+    """Return bash commands to concatenate files belonging to the same 
+    simulation and variable using CDO, and bash commands that remove the 
+    original files.
+    """
+
+    F = cluster_time_slices(path)
+
     # Concatenate files belonging to the same simulation.
     for keys, files in F.walk():
         if len(files) == 1:
@@ -61,7 +75,6 @@ def concatenate(path):
         d['period'] = '-'.join((n1, n2))
         ofile = esg.fn_from(**d)
         
-#        cmds.append("ncrcat " + ' '.join(files) + ' ' + ofile )
         cmds.append("cdo cat " + ' '.join(files) + ' ' + ofile )
         rm.append("rm " + ' '.join(files))
 
@@ -69,19 +82,21 @@ def concatenate(path):
     
 
 def monthly_clim(ifile, ofile=None, years=None, tag=''):
-    """Compute the monthly climatology from a file over the specified years.
+    """Return bash commands to compute the monthly climatology from a file or 
+    a sequence of files over the specified years using CDO. 
     
     The function returns a call to cdo operator ymonavg.
         
     Parameters
     ----------
     ifile : str
-      Path to input file.
+      Path to input file or sequence of paths. 
     ofile : str
       Path to output file, if None, a file name will be created in the
       current directory.
     years : (y1, y2) 
-      Tuple of start and end years.
+      Tuple of start and end years. If None compute the climatology over the 
+      complete time span.
     tag : str
        Identifying tag used when generating `ofile` to identify individual 
        periods. Useful if many climatologies are computed for the same experiment. 
@@ -93,32 +108,69 @@ def monthly_clim(ifile, ofile=None, years=None, tag=''):
     'cdo ymonavg -seldate,2040-01-01,2069-12-31 ../data/sic_OImon_CCSM4_rcp85_r1i1p1_200601-210012.nc sic_OImon_CCSM4_rcp85_r1i1p1_204001-206912-clim<f50>.nc'
     
     """
-    a1, a2, clim = esg.fn_date(esg.fn_split(ifile)['period'])
-    if clim:
-        return
+    import collections
 
-    r = range(a1.year,a2.year+1)
+    if type(ifile) == str:
+        ifile = [ifile,]
+
+    # Check that all files belong to the same simulation.
+    S = collections.defaultdict(set)
+    for f in ifile:
+        for key, val in esg.fn_split(f).items():
+            S[key].add(val)
+    for key, val in S.items():
+        if len(val) > 1 and key != 'period':
+            raise ValueError("Files don't all belong to the same simulation and variable.")
+
+
+    # Find the start and end dates of the input files    
+    starts, ends, clims = zip(*[esg.fn_date(esg.fn_split(f)['period']) for f in ifile])
+    if any(clims):
+        raise ValueError("Climatology file found.")
+    start, end = starts[0], ends[-1]
+
+
+    # If years is a slice, convert to a tuple of years using the range between
+    # the start and end dates. 
+    r = range(start.year, end.year+1)
     if years:
         if type(years) == slice:
             years = r[years][0], r[years][-1]
 
+
+    # Select only the files that fall within that range.
+    infile = []
+    for i, f in enumerate(ifile):
+        if ends[i].year > years[0] and starts[i].year < years[1]:
+            infile.append(f)
+
+    if len(infile) == 0:
+        return 
+
+    # Create output file if none is given. 
     if ofile is None:
-        d = esg.fn_split(ifile)
+        d = esg.fn_split(infile[0])
         if years:
             d['period'] = "{0}01-{1}12".format(*years)
-        
+        else:
+            d['period'] = "{0}01-{1}12".format(start, end)
+
         ofile = esg.fn_from(clim='clim{0}'.format(tag), **d)
-        
+
+    # Create the CDO commands with or without inline concatenation.
+    cmd = ["cdo", "ymonavg", "-f", "nc4", "-z", "zip", ]
+
     if years:
         y1,y2 = years
-           
-        start = "{0}-01-01".format(y1)
-        end = "{0}-12-31".format(y2)
-        cmd = "cdo ymonavg -f nc4 -z zip -seldate,{0},{1} {2} {3}".format(start, end, ifile, ofile)
-    	
-    else:
-        cmd = "cdo ymonavg -f nc4 -z zip    {0} {1}".format(ifile, ofile)
+        cmd.append("-seldate,{0}-01-01,{1}-12-31".format(y1, y2))
 
-    return cmd
+    if len(infile) > 1: 
+        cmd.extend(["-cat", ] + infile)
+    else:
+        cmd.append(infile[0])
+
+    cmd.append(ofile)
+
+    return ' '.join(cmd)
 
     
